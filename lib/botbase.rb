0dad2a5a19ba66906805131a5cd3fedc85b79fb0
Your Name <you@example.com>
2017-10-14 17:47:02 +0100

    feature: The notification object can now be passed to plugin modules through a callback parameter.

--- lib/botbase.rb
+++ lib/botbase.rb
@@ -9,22 +9,34 @@ require 'simple-config'
 
 class BotBase
   
-  def initialize(config=nil, botname: 'Nicole', notifier: nil)
+  def initialize(config=nil, botname: 'Nicole', notifier: nil, debug: false)
 
     @h = nil
 
+    @botname, @notifier, @debug = botname, notifier, debug
+    
     if config then
       
       @h = SimpleConfig.new(config).to_h      
-
+      puts 'j:' + @h.inspect
       # load the service modules
       @modules = initialize_modules(@h[:modules])
       
     end
-    
-    @botname, @notifier = botname, notifier
 
   end
+  
+  # used for display debug messages from modules
+  #
+  def debug(msg)
+    notice 'botbase/debug: ' + msg if @debug
+  end
+  
+  # used for display debug messages from modules
+  #
+  def notice(msg)
+    @notifier.notice msg if @notifier
+  end  
   
   def received(sender='user01', msg, mode: :voicechat, echo_node: 'node1')
 
@@ -33,13 +45,16 @@ class BotBase
     
     r = nil
     
-    msg_recognised = @modules.detect do |m| 
-      r = m.query(msg, mode: mode, echo_node: echo_node); r and r.length > 0 
+    detected = @modules.detect do |m| 
+      r = m.query(msg, mode: mode, echo_node: echo_node)
+      r and r.length > 0 
     end
     
-    return r if msg_recognised
-    
-    ''
+    if detected then
+      r 
+    else
+      ''
+    end
 
   end
 
@@ -47,7 +62,7 @@ class BotBase
 
     puts 'restarting ...'
     @modules = initialize_modules(@h[:modules]) if @h
-    @notifier.notice "echo: #{@botname} is now ready" if @notifier
+    notice "echo: #{@botname} is now ready"
           
   end
   
@@ -58,10 +73,13 @@ class BotBase
     modules.inject([]) do |r, m|
       
       name, settings = m
+      
+      debug 'initialising botbase-module-'  + name.to_s
             
       klass_name = 'BotBaseModule' + name.to_s
 
-      r << Kernel.const_get(klass_name).new(settings)
+      
+      r << Kernel.const_get(klass_name).new(settings.merge({callback: self}))
 
     end
         
