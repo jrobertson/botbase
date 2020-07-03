22ccab2831f51a161f80729b83e669f5a0c67df6
James Robertson <james@jamesrobertson.eu>
2020-07-03 15:51:14 +0100

    feature: Channel lock allows dialogue with a specific module. Message_prefix can help narrow it down to a single transaction.

--- botbase.gemspec
+++ botbase.gemspec
@@ -1,12 +1,12 @@
 Gem::Specification.new do |s|
   s.name = 'botbase'
-  s.version = '0.2.0'
+  s.version = '0.2.1'
   s.summary = 'Provides bot functionality primarily for use by the ' + 
       'sps_bot gem.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/botbase.rb']
   s.add_runtime_dependency('mtlite', '~> 0.3', '>=0.3.5')
-  s.add_runtime_dependency('simple-config', '~> 0.6', '>=0.6.3')
+  s.add_runtime_dependency('simple-config', '~> 0.6', '>=0.6.4')
   s.signing_key = '../privatekeys/botbase.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
--- lib/botbase.rb
+++ lib/botbase.rb
@@ -10,42 +10,75 @@ require 'simple-config'
 class BotBase  
   
   attr_reader :log
+  attr_accessor :channel_lock, :message_prefix
   
-  
-  def initialize(config=nil, botname: 'Nicole', notifier: nil, log: nil)
+  def initialize(config=nil, botname: 'Nicole', notifier: nil, log: nil, 
+                 debug: false)
 
-    @botname, @notifier, @log, @h = botname, notifier, log, nil
+    @botname, @notifier, @log, @h, @debug = botname, notifier, log, nil, debug
     
     if config then
       
       @h = SimpleConfig.new(config).to_h      
+      puts '@h: ' + @h.inspect if @debug
       @modules = initialize_modules(@h[:modules])
       
     end
 
   end
   
-  # used for display debug messages from modules
+  # displays debug messages from modules
   #
   def notice(msg)
     @notifier.notice msg if @notifier
+    puts msg
   end  
   
   def received(sender='user01', msg, mode: :voicechat, echo_node: 'node1')
 
     msg.rstrip!
     
+    if msg.downcase == 'exit' then
+      @channel_lock, @message_prefix = nil, nil
+    end
+    
     log.info 'BotBase/received: ' + msg if log
     self.restart if msg == @botname + ' restart'
     
     r = nil
     
-    detected = @modules.detect do |m| 
-      r = m.query(msg, mode: mode, echo_node: echo_node)
+    detected = if @channel_lock then
+    
+      if @debug then
+        notice 'botbase: inside channel locked'
+        notice 'botbase: fullmsg:' + (@message_prefix.to_s + msg).inspect
+      end
+      
+      r = @modules[@channel_lock].query(@message_prefix.to_s + msg, 
+                                        mode: mode, echo_node: echo_node)
+      puts 'r: ' + r.inspect if @debug
       r and r.length > 0 
+      
+    else
+      
+      if @debug then
+        puts 'before modules.detect'
+        puts '@modules.values: ' + @modules.values\
+            .map {|x| x.inspect[0..100] }.join("\n")
+      end
+    
+      @modules.detect do |name, obj|
+        puts 'name: ' + name.inspect if @debug
+        r = obj.query(msg, mode: mode, echo_node: echo_node)
+        r and r.length > 0 
+      end
+      
     end
     
+    
     if detected then
+      
+      puts 'detected: ' + detected.inspect[0..200] + ' ...' if @debug
       
       if mode == :voicechat then
         MTLite.new(r).to_s.gsub(/ +https:\/\/[\S]+/,'')
@@ -71,9 +104,8 @@ class BotBase
   
   def initialize_modules(modules)
     
-    modules.inject([]) do |r, m|
+    a = modules.map do |name, settings|
       
-      name, settings = m
       settings = {} if settings.is_a? String
       
       if log then
@@ -83,9 +115,14 @@ class BotBase
             
       klass_name = 'BotBaseModule' + name.to_s
       
-      r << Kernel.const_get(klass_name).new(settings.merge({callback: self}))
+      [name, Kernel.const_get(klass_name).new(settings.merge({callback: self, 
+                                                              debug: @debug}))]
 
     end
+    
+    puts 'a: ' + a[0..100].inspect if @debug
+    a.to_h
+    
         
   end  
     
