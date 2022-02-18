78410d5c888f3eb86e3898a91a37cc8e3cbea561
James Robertson <digital.robertson@gmail.com>
2022-02-18 21:22:33 +0000

    bug fix: Now works with Ruby 3.0.2.

--- botbase.gemspec
+++ botbase.gemspec
@@ -1,15 +1,15 @@
 Gem::Specification.new do |s|
   s.name = 'botbase'
-  s.version = '0.2.1'
+  s.version = '0.2.2'
   s.summary = 'Provides bot functionality primarily for use by the ' + 
       'sps_bot gem.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/botbase.rb']
-  s.add_runtime_dependency('mtlite', '~> 0.3', '>=0.3.5')
-  s.add_runtime_dependency('simple-config', '~> 0.6', '>=0.6.4')
+  s.add_runtime_dependency('mtlite', '~> 0.4', '>=0.4.1')
+  s.add_runtime_dependency('simple-config', '~> 0.7', '>=0.7.2')
   s.signing_key = '../privatekeys/botbase.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
-  s.email = 'james@jamesrobertson.eu'
+  s.email = 'digital.robertson@gmail.com'
   s.homepage = 'https://github.com/jrobertson/botbase'
 end
--- lib/botbase.rb
+++ lib/botbase.rb
@@ -7,79 +7,79 @@ require 'mtlite'
 require 'simple-config'
 
 
-class BotBase  
-  
+class BotBase
+
   attr_reader :log
   attr_accessor :channel_lock, :message_prefix
-  
-  def initialize(config=nil, botname: 'Nicole', notifier: nil, log: nil, 
+
+  def initialize(config=nil, botname: 'Nicole', notifier: nil, log: nil,
                  debug: false)
 
     @botname, @notifier, @log, @h, @debug = botname, notifier, log, nil, debug
-    
+
     if config then
-      
-      @h = SimpleConfig.new(config).to_h      
+
+      @h = SimpleConfig.new(config).to_h
       puts '@h: ' + @h.inspect if @debug
       @modules = initialize_modules(@h[:modules])
-      
+
     end
 
   end
-  
+
   # displays debug messages from modules
   #
   def notice(msg)
     @notifier.notice msg if @notifier
     puts msg
-  end  
-  
+  end
+
   def received(sender='user01', msg, mode: :voicechat, echo_node: 'node1')
 
     msg.rstrip!
-    
+
     if msg.downcase == 'exit' then
       @channel_lock, @message_prefix = nil, nil
     end
-    
+
     log.info 'BotBase/received: ' + msg if log
     self.restart if msg == @botname + ' restart'
-    
+
     r = nil
-    
+
     detected = if @channel_lock then
-    
+
       if @debug then
         notice 'botbase: inside channel locked'
         notice 'botbase: fullmsg:' + (@message_prefix.to_s + msg).inspect
       end
-      
-      r = @modules[@channel_lock].query(@message_prefix.to_s + msg, 
+
+      r = @modules[@channel_lock].query(@message_prefix.to_s + msg,
                                         mode: mode, echo_node: echo_node)
       puts 'r: ' + r.inspect if @debug
-      r and r.length > 0 
-      
+      r and r.length > 0
+
     else
-      
+
       if @debug then
         puts 'before modules.detect'
         puts '@modules.values: ' + @modules.values\
             .map {|x| x.inspect[0..100] }.join("\n")
       end
-    
+
       @modules.detect do |name, obj|
         puts 'name: ' + name.inspect if @debug
         r = obj.query(msg, mode: mode, echo_node: echo_node)
-        r and r.length > 0 
+        r and r.length > 0
       end
-      
+
     end
-    
-    
+
+
     if detected then
-      
+
       puts 'detected: ' + detected.inspect[0..200] + ' ...' if @debug
-      
+
       if mode == :voicechat then
         MTLite.new(r).to_s.gsub(/ +https:\/\/[\S]+/,'')
       else
@@ -97,33 +97,33 @@ class BotBase
     log.info 'BotBase/restart: restarting ...' if log
     @modules = initialize_modules(@h[:modules]) if @h
     notice "echo: #{@botname} is now ready"
-          
+
   end
-  
+
   private
-  
+
   def initialize_modules(modules)
-    
+
     a = modules.map do |name, settings|
-      
+
       settings = {} if settings.is_a? String
-      
+
       if log then
-        log.info 'BotBase/initialize_modules: ' + 
+        log.info 'BotBase/initialize_modules: ' +
             'initialising botbase-module-'  + name.to_s
       end
-            
+
       klass_name = 'BotBaseModule' + name.to_s
-      
-      [name, Kernel.const_get(klass_name).new(settings.merge({callback: self, 
+
+      [name, Kernel.const_get(klass_name).new(**settings.merge({callback: self,
                                                               debug: @debug}))]
 
     end
-    
+
     puts 'a: ' + a[0..100].inspect if @debug
     a.to_h
-    
-        
-  end  
-    
+
+
+  end
+
 end
